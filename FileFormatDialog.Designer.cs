ef0b73e77fba411b72bc8c09c0fe864990a3d5c1
nafef60552 <nafef60552@qianhost.com>
2023-10-29 11:04:30 +0200

    add file format dialog form

---
+++ FileFormatDialog.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace L55
+{
+    partial class FileFormatDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCsv = new System.Windows.Forms.Button();
+            this.btnJson = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCsv
+            // 
+            this.btnCsv.Location = new System.Drawing.Point(134, 12);
+            this.btnCsv.Name = "btnCsv";
+            this.btnCsv.Size = new System.Drawing.Size(104, 52);
+            this.btnCsv.TabIndex = 0;
+            this.btnCsv.Text = "Csv";
+            this.btnCsv.UseVisualStyleBackColor = true;
+            this.btnCsv.Click += new System.EventHandler(this.btnCsv_Click_1);
+            // 
+            // btnJson
+            // 
+            this.btnJson.Location = new System.Drawing.Point(12, 12);
+            this.btnJson.Name = "btnJson";
+            this.btnJson.Size = new System.Drawing.Size(116, 52);
+            this.btnJson.TabIndex = 1;
+            this.btnJson.Text = "Json";
+            this.btnJson.UseVisualStyleBackColor = true;
+            this.btnJson.Click += new System.EventHandler(this.btnJson_Click_1);
+            // 
+            // FileFormatDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 78);
+            this.Controls.Add(this.btnJson);
+            this.Controls.Add(this.btnCsv);
+            this.Name = "FileFormatDialog";
+            this.Text = "FileFormatDialog";
+            this.Load += new System.EventHandler(this.FileFormatDialog_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCsv;
+        private System.Windows.Forms.Button btnJson;
+    }
+}
---
+++ FileFormatDialog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L55
+{
+    public enum DialogMode
+    {
+        Save,
+        Load
+    }
+
+    public partial class FileFormatDialog : Form
+    {
+        public DialogMode Mode { get; set; }
+
+        public event Action<string> FormatSelected;
+
+        public FileFormatDialog(DialogMode mode)
+        {
+            InitializeComponent();
+            Mode = mode;
+            if (mode == DialogMode.Save)
+            {
+                btnCsv.Text = "Save to *.csv (*.txt) file";
+                btnJson.Text = "Save to *.json file";
+            }
+            else
+            {
+                btnCsv.Text = "Load from *.csv (*.txt) file";
+                btnJson.Text = "Load from *.json file";
+            }
+        }
+
+        public FileFormatDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void FileFormatDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJson_Click_1(object sender, EventArgs e)
+        {
+            FormatSelected?.Invoke(".json");
+            Close();
+        }
+
+        private void btnCsv_Click_1(object sender, EventArgs e)
+        {
+            FormatSelected?.Invoke(".csv");
+            Close();
+        }
+    }
+
+}
